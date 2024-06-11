c278ddb955097efd2ccd78bb8851dcbd011ada77
aduodu <angieduodu3@gmail.com>
2024-06-11 19:20:11 -0400

    Completed Directions, attempting to submit.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import "./About.css";
+import picture from "../assets/picture.jpeg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <p><div>
+<div className="split left">
+<div className="centered">
+<img 
+className="profile_image" src={picture}
+alt="Profile Pic"
+></img>
+</div>
+</div>
+<div className="split right">
+<div className="centered">
+<div className="name_title">Angela</div>
+<div className="brief_description"> Hi! My name is Angela; rising junior majoring in computer science. 
+  <div>I really like art, music, and food. Cooking and painting are my favorite hobbies. I also recently tried pottery.</div>
+</div>
+</div>
+</div>
+</div>
+</p> 
       </div>
     )
   }
